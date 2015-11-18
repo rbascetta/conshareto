06d5a5a95429f89e74de876b44ba91049313b2a9
Andrew Anissi <andrew@wingedearth.com>
2015-11-18 15:05:24 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express       = require('express');
+var path          = require('path');
+var favicon       = require('serve-favicon');
+var logger        = require('morgan');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+var session       = require('express-session');
+var locus         = require('locus');
+
+// load the env variables
+require('dotenv').load();
