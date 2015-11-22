7209ea589195ca8bafe5b70b73d6cedd0ce44304
Andrew Anissi <andrew@wingedearth.com>
2015-11-21 16:13:15 -0800

    re-creating server.js

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
 # conshareto
 
 # Links
-<a href="https://trello.com/b/8NnxlvTx/conshareto">Trello</a>
+<a href="https://trello.com/b/8NnxlvTx/conshareto">Trello</a><br>
 <a href="https://github.com/wingedearth/conshareto">Github</a>
 
 # Description
 
+
+
 # Technologies Used
+
+- Node.js
+- Express.js
+- Javascript
+- HTML5
+- CSS3
+- OAuth/Passport
+- APIs
+
+
+# APIs accessed
+- Ticketmaster
+
 
 # Wireframes
 
-# ERDs
+
+# Entity Relationship Diagrams (ERDs)
+
 
 # User Stories
+
+## Sprint
+
+
+## Icebook
+
--- server.js
+++ server.js
@@ -1,55 +1,17 @@
-var express       = require('express');
-var path          = require('path');
-var favicon       = require('serve-favicon');
-var logger        = require('morgan');
-var cookieParser  = require('cookie-parser');
-var bodyParser    = require('body-parser');
-var session       = require('express-session');
-var locus         = require('locus');
-var passport      = require('passport');
+var express      = require('express'); // use express framework
+var path         = require('path'); // handle & transform file paths
+var logger       = require('morgan'); // log actions in console for dev
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var routes       = require('./routes/index'); // import routes from file
+var app          = express(); // assign "app" to express functions.
+var debug        = require('debug')('app:http');
+var session      = require('express-session'); // for login sessions
+var passport     = require('passport'); // Easy API Authorization
 
-// load the env variables
+// secure keys
 require('dotenv').load();
-
-
-// connect to the MongoDB with mongoose
-require('./config/database');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-require('ejs').delimiter = '$';
-
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// mount passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// you can now write css in scss files!
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false,
-  sourceMap: false
-}));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// configure passport
-require('./config/passport')(passport);
-
-// index mounts all routes
-require('./routes/index')(app);
-
-// invalid request, send 404 page
-app.use(function(req, res) {
-  res.status(404).send('Thanks for the request, but I can`t find it.');
-});
-
-module.exports = app;
