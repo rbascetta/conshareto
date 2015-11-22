22a4c4886bd983fd0f78c261ec5d4f706f6499b5
Tara Strauss <tara.m.strauss@gmail.com>
2015-11-21 16:37:35 -0800

    creates basic MVP models

--- models/events.js
+++ models/events.js
@@ -1,1 +1,26 @@
-events.js
+var mongoose = require('mongoose');
+
+// The factSchema is used to embedded docs in as tudent doc.
+// There is no model and no 'facts' collection
+
+var venueSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  city: String,
+  state: String,
+  zipcode: String,
+  venueUrl: String
+});
+
+var artistSchema = new mongoose.Schema({
+  name: String
+});
+
+var eventSchema = new mongoose.Schema({
+  artists: [artistSchema],
+  date: Date,
+  venues: [venueSchema],
+  ticketUrl: String
+});
+
+module.exports = mongoose.model('Event', eventSchema);
