0ebfadf2f7c308d9785035e922886252c7d1f62e
Andrew Anissi <andrew@wingedearth.com>
2015-11-21 17:10:12 -0800

    generate express app template

--- server.js
+++ server.js
@@ -15,3 +15,32 @@ require('dotenv').load();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+require('ejs').delimiter = '$';
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// mount passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// write css in scss files!
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false,
+  sourceMap: false
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
