c7cc8234ffe13747d90a4f771bff340831de3c06
Tara Strauss <tara.m.strauss@gmail.com>
2015-11-21 18:57:09 -0800

    adds google oauth

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var path         = require('path'); // handle & transform file paths
 var logger       = require('morgan'); // log actions in console for dev
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-var routes       = require('./routes/index'); // import routes from file
-var app          = express(); // assign "app" to express functions.
 var debug        = require('debug')('app:http');
 var session      = require('express-session'); // for login sessions
 var passport     = require('passport'); // Easy API Authorization
-
+var mongoose     = require('./config/database');
+//var routes       = require('./routes/index')(app, passport);
+var users        = require('./routes/users');
+var app          = express(); // assign "app" to express functions.
 // secure keys
 require('dotenv').load();
 
@@ -19,13 +20,19 @@ app.set('view engine', 'ejs');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-require('ejs').delimiter = '$';
+require('ejs').delimiter = '%';
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: 'Tony and the Ultras are Ultra!',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // mount passport
 app.use(passport.initialize());
@@ -40,6 +47,12 @@ app.use(require('node-sass-middleware')({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+require('./config/passport')(passport);
+
+require('./routes/index')(app, passport);
+
+module.exports = app;
+
 
 
 
