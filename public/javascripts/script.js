360258e9fcc0f95340d1704edafe8aa46e74d715
alee <alee046@ucr.edu>
2015-11-24 15:57:35 -0800

    searchResult array

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,22 @@
-// $('.attend-event').on('click', function(el) {
+var searchResults=[];
+$('.attend-event').on('click', function(el) {
 
- //  var  eventId
- // if the event is already in database
- //  set eventId = the id
- // create new event and set eventId equal to the id
- // send eventId to user list of events
-// });
+  var  eventId
+ if the event is already in database
+  set eventId = the id
+ create new event and set eventId equal to the id
+ send eventId to user list of events
+});
 
 
 
-// $('#search').on('click', function(el){
+$('#search').on('click', function(el){
   $.ajax({
     url: '/search',
     method: 'POST',
     data: {zip:$('').val(), radius:$('').val()}
   }).done(function(events){
+    searchResults = events;
     events.forEach(function(event){
       $('body').append(
         '<div id=' + event.Id + '>' + 'Date: ' + event.Date
@@ -23,4 +25,5 @@
         );
     });
   });
-// })
+})
+
