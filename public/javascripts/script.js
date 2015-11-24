46845f6a19f8cf36e97a7657ee009d6711f77d94
alee <alee046@ucr.edu>
2015-11-24 15:14:14 -0800

    append all events to index

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,26 @@
-$('.attend-event').on('click', function(el) {
+// $('.attend-event').on('click', function(el) {
 
  //  var  eventId
  // if the event is already in database
  //  set eventId = the id
  // create new event and set eventId equal to the id
  // send eventId to user list of events
-});
+// });
 
 
 
-$('#search').on('click', function(el){
+// $('#search').on('click', function(el){
   $.ajax({
     url: '/search',
-    method: 'GET',
+    method: 'POST',
     data: {zip:$('').val(), radius:$('').val()}
-  }).done(function(data){
-
-  })
-})
+  }).done(function(events){
+    events.forEach(function(event){
+      $('body').append(
+        '<div id=' + event.Id + '>' + 'Date: ' + event.Date
+        + ' Venue: '+ event.Venue.Name + ' Artist: '+ event.Artists[0].Name
+        +'</div>'
+        );
+    });
+  });
+// })
