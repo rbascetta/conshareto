9bcfc823945f0cee1218352bdd5dfb0c0010623c
Andrew Anissi <andrew@wingedearth.com>
2015-11-24 11:01:55 -0800

    add route for search engine

--- routes/index.js
+++ routes/index.js
@@ -5,56 +5,86 @@ var userController  = require('../controllers/users');
 var eventController = require('../controllers/events');
 var request         = require('request');
 
+// load environmental variables
 require('dotenv').load();
 
-// Jambase variables
-  // create Jambase date variable
-    var currentFullDate = new Date();
-    var currentYear  = currentFullDate.getFullYear();
-    var currentMonth = currentFullDate.getMonth() + 1;
-    var currentDay   = currentFullDate.getDate();
-    var currentDate  = currentYear + "-" + currentMonth + "-" + currentDay;
+// Determine search parameters
+ // create start date variables
+ var today        = new Date();
+ var currentYear  = today.getFullYear();
+ var currentMonth = today.getMonth() + 1;
+ var currentDay   = today.getDate();
+ var currentDate  = currentYear + "-" + currentMonth + "-" + currentDay;
 
-  // assign HTTP request variables
-  var baseUri         = "http://api.jambase.com/events";
-  var zipPrefix       = "?zipCode=";
-  var zip;
-  var radiusPrefix    = "&radius=";
-  var startDate       = "&startDate=" + currentDate;
-  var endDatePrefix   = "&endDate=";
-  var endDate;
-  var jamKey          = "&api_key=" + process.env.JAMBASE_KEY;
+ // set end date equal to one month after start date
+ var endDay       = currentDay;
+ var endMonth     = currentMonth + 1;
+ var endYear      = currentYear;
+ if ((endMonth=="13") || (endMonth==13)) {
+   endMonth = 1;
+   endYear += 1;
+ };
+ var endDate      = endYear + "-" + endMonth + "-" + endDay;
 
-router.get('/events', function(req, res, next){
-  console.log(req.body);
-  uri = baseUri + zipPrefix + zip + radiusPrefix + startDatePrefix
-  + startDate + endDatePrefix + endDate + jamKey;
-
-})
 module.exports = function(app, passport) {
+
+  // Search Jambase API per client request
+  app.post('/search', function(req, res, next){
+    console.log(req.body);
+
+    // assign HTTP request variables
+    var baseUri        = "http://api.jambase.com/events";
+    var jamKey         = "&api_key=" + process.env.JAMBASE_KEY;
+    var zip            = encodeURIcomponent(req.body.distance.zip);
+    var zipParam       = "?zipCode=" + zip;
+    var radius         = encodeURIcomponent(req.body.distance.radius);
+    var radiusParam    = "&radius=" + radius;
+    var distanceParams = zipParam + radiusParam;
+    var startParam     = "&startDate=" + currentDate;
+    var endParam       = "&endDate=" + endDate;
+    var dateParams     = startParam + endParam;
+
+    // build full URI for http request to Jambase API
+    uri = baseUri + distanceParams + dateParams + jamKey;
+
+    console.log("Attempting to connect to: ", uri);
+
+    // send http request to the Jambase API
+    request.get(uri, function(err, response, body) {
+      var body = JSON.parse(body);
+
+      // Call res.send in the API request's callback*!
+      res.send(body.events);
+    });
+  });
+
+  /* GET home page. */
+
   app.get('/', function(req, res, next) {
     res.render('index', { title: "conShareto", user: req.user });
   });
 
-  app.get('/auth/google', passport.authenticate(
-    'google',
-    { scope: ['profile', 'email'] }
-  ));
 
-  app.get('/oauth2callback', passport.authenticate(
-    'google',
-    { successRedirect : '/', failureRedirect : '/' }
-  ));
+  // Google Authentication / Login
+    app.get('/auth/google', passport.authenticate(
+      'google',
+      { scope: ['profile', 'email'] }
+    ));
 
-  app.get('/logout', function(req, res){
-    req.logout(); res.redirect('/');
-  });
+    app.get('/oauth2callback', passport.authenticate(
+      'google',
+      { successRedirect : '/', failureRedirect : '/' }
+    ));
+
+    app.get('/logout', function(req, res){
+      req.logout(); res.redirect('/');
+    });
 
   // return all users
-  router.get('/users', userController.all);
+  app.get('/users', userController.all);
 
   // return all events
-  router.get('/events', eventController.all);
+  app.get('/myevents', eventController.all);
 
   // mount API router
   app.use('/api', router);
