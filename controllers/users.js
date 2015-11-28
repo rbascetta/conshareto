4b36d566988cd3d751e66818e89281ef67b0d6ae
Tara Strauss <tara.m.strauss@gmail.com>
2015-11-28 02:23:32 -0800

    cleaner code

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,6 @@ module.exports = {
   },
 
   myAttendEvents: function (req, res) {
-
     var myEventObjects = [];
     var myEvents = req.user.myEvents;
     var myAttendEvents = myEvents.filter(function(object, index){ return (myEvents[index].attending === true); })
@@ -23,7 +22,6 @@ module.exports = {
         }
       });
     }
-
   },
 
    myFollowEvents: function (req, res) {
@@ -38,49 +36,6 @@ module.exports = {
         }
       });
     }
-
   }
 
 };
-
-    // var myAttendEventObjects = [];
-    // var myAttendEvents = req.user.myEvents;
-    // for (var i = 0; i < myAttendEvents.length; i++) {
-    //   Event.findOne({_id: myAttendEvents[i].eventId}, function (err, event) {
-    //       myAttendEventObjects.push(event);
-    //       if (myAttendEventObjects.length === myAttendEvents.length) {
-    //         console.log(myAttendEventObjects.filter(function(object, index){ return (myAttendEvents[index].attending === true); }));
-    //         res.send(myAttendEventObjects.filter(function(object, index ){ return (myAttendEvents[index].attending === true); }));
-    //       }
-    //   });
-    // }
-
-        // var myAttendEventObjects = [];
-    // var myAttendEvents = req.user.myEvents;
-    // var arrayCounter = 0;
-    // for (var i = 0; i < myAttendEvents.length; i++) {
-    //   Event.findOne({_id: myAttendEvents[i].eventId}, function (err, event) {
-    //       if (myAttendEvents[arrayCounter].attending === true) {
-    //         myAttendEventObjects.push(event);
-    //         if (arrayCounter === myAttendEvents.length-1) {
-    //          // console.log(myAttendEventObjects.filter(function(object, index){ return (myAttendEvents[index].attending === true); }));
-    //           console.log(myAttendEventObjects);
-    //           res.send(myAttendEventObjects);
-    //         }
-    //       }
-    //   });
-    //   arrayCounter++;
-    // }
-
-    //     console.log('hi');
-    // var myEventObjects = [];
-    // var myEvents = req.user.myEvents;
-    // for (var i = 0; i < myEvents.length; i++) {
-    //   Event.findOne({_id: myEvents[i].eventId}, function (err, event) {
-    //       myEventObjects.push(event);
-    //       if (myEventObjects.length === myEvents.length) {
-    //         console.log(myEventObjects.filter(function(object, index){ return (myEvents[index].following === true); }));
-    //         res.send(myEventObjects.filter(function(object, index ){ return (myEvents[index].following === true); }));
-    //       }
-    //   });
-    // }
